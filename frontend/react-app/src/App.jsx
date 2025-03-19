6b4803228b8f841b1d1d4304cbca99d99de1e569
AdityaKannur <adityakannur28@gmail.com>
2025-03-19 11:59:49 +0530

    debug

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Roles from "./components/Roles";
-import PatientDashboard from './pages/PatientDashboard'
+import PatientDashboard from "./pages/PatientDashboard";
 import InsurerDashboard from "./pages/InsurerDashboard";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/roles" element={<Roles />} />
         <Route path="/patient-dashboard" element={<PatientDashboard />} />
         <Route path="/insurer-dashboard" element={<InsurerDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
